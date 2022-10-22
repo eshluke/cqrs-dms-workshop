9aafe43216ffef5416b6235eb3f07a56abb3b877
Eunju Rho <ej.rho35@gmail.com>
2022-10-23 00:58:39 +0900

    added lib/aurora-snapshot.ts

--- bin/cqrs-workshop.ts
+++ bin/cqrs-workshop.ts
@@ -3,7 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { VpcStack } from '../lib/vpc';
-import { Aurora as AuroraStack } from '../lib/aurora';
+import { Aurora as AuroraStack } from '../lib/aurora-snapshot';
 
 const app = new cdk.App();
 
