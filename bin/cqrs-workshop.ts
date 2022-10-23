9e32a3518366f5c7f55e29ec5d45cc440ea3a069
Eunju Rho <ej.rho35@gmail.com>
2022-10-23 18:39:32 +0900

    lookup vpc

--- bin/cqrs-workshop.ts
+++ bin/cqrs-workshop.ts
@@ -8,7 +8,7 @@ import { Aurora as AuroraStack } from '../lib/aurora-snapshot';
 
 const app = new cdk.App();
 
-const vpcStack = new VpcStack(app, 'VpcStack');
+const vpcStack = new VpcStack(app, 'VpcStack', { vpcName: 'project-vpc' });
 
 const targetDbStack = new AuroraStack(app, 'TargetDbStack', { 
   description: "Workshop Target DB Stack",
--- lib/vpc.ts
+++ lib/vpc.ts
@@ -2,29 +2,28 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
+export interface VpcStackProps extends cdk.StackProps {
+  
+  /**
+   * the name of the VPC
+   *
+   * @type {string}
+   * @memberof VpcStackProps
+   */
+  readonly vpcName: string;
+}
+
 export class VpcStack extends cdk.Stack {
 
-  public readonly vpc: ec2.Vpc;
+  public readonly vpc: ec2.IVpc;
   public readonly publicSubnets: ec2.SelectedSubnets;
   public readonly privateSubnets: ec2.SelectedSubnets;
 
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: VpcStackProps) {
     super(scope, id, props);
 
-    this.vpc = new ec2.Vpc(this, 'WorkshopVpc', {
-      vpcName: 'CqrsWorkshopVpc',
-      cidr: '10.1.0.0/16',
-      maxAzs: 2,
-      subnetConfiguration: [
-        {
-          name: 'public',
-          subnetType: ec2.SubnetType.PUBLIC,
-        },
-        {
-          name: 'private',
-          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-        },
-      ]
+    this.vpc = ec2.Vpc.fromLookup(this, 'ProjectVpc', {
+      vpcName: props.vpcName,
     });
 
     this.publicSubnets = this.vpc.selectSubnets({
