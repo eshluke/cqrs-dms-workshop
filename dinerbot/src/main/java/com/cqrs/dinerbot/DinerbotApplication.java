7d9ac814d206b2307b9cd00786e3faebf8a5c34b
Eunju Rho <ej.rho35@gmail.com>
2022-10-25 14:10:00 +0900

    added dinerbot directory

---
+++ dinerbot/src/main/java/com/cqrs/dinerbot/DinerbotApplication.java
@@ -0,0 +1,13 @@
+package com.cqrs.dinerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinerbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinerbotApplication.class, args);
+	}
+
+}
---
+++ dinerbot/src/test/java/com/cqrs/dinerbot/DinerbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cqrs.dinerbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinerbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
