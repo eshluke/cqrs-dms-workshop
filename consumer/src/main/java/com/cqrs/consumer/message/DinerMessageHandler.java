9b9770ef815afd21222199302b2e821f5bd10981
Eunju Rho <ej.rho35@gmail.com>
2022-10-26 09:54:44 +0900

    added comments on DinerMessageHandler

--- consumer/src/main/java/com/cqrs/consumer/message/DinerMessageHandler.java
+++ consumer/src/main/java/com/cqrs/consumer/message/DinerMessageHandler.java
@@ -11,5 +11,12 @@ public class DinerMessageHandler {
 
     public void onMessage(DmsMessage message) {
         log.info("metadata: {}, paylod: {}", message.getMetadata(), message.getPayload());
+        // 메세지 타입 분류하여 적절한 핸들러에 위임 (Full Load vs CDC)
+        // Full Load 의 경우: CREATE-TABLE 과 DROP-TABLE 타입
+        //   --> 예: Full Load 메세지가 들어온 경우 시스템을 종료한다 (예외)
+        // CDC 의 경우: INSERT, UPDATE, DELETE 타입 핸들링
+        //   --> 예: 1. 계약 아이디를 꺼낸다.
+        //          2. 조회 모델을 구성하기 위해 Target DB 의 원본 테이블들을 Join 하여 조회한다.
+        //          3. 이를 Target DB 의 조회 모델에 저장한다.
     }
 }
