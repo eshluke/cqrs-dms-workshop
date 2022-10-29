53295605cc8d7e080fdb3da8da2483e4c4d0b5c4
Eunju Rho <ej.rho35@gmail.com>
2022-10-29 15:34:25 +0900

    removed unused stack: dmsStack

--- cdk/bin/cqrs-workshop.ts
+++ cdk/bin/cqrs-workshop.ts
@@ -5,7 +5,6 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as rds from 'aws-cdk-lib/aws-rds';
 import { Aurora as AuroraStack } from '../lib/aurora';
 import { KafkaStack } from '../lib/kafka';
-import { DmsStack } from '../lib/dms';
 import { FargateStack } from '../lib/fargate';
 
 const VPC_ID = '< REPLACE >';
@@ -55,11 +54,6 @@ const kafkaStack = new KafkaStack(app, 'KafkaStack', {
   vpcId: VPC_ID,
   vpcCidr: VPC_CIDR,
   privateSubnetIds: PRIVATE_SUBNET_IDS,
-});
-
-const dmsStack = new DmsStack(app, 'DmsStack', {
-  sourceDbSecret: sourceDbStack.generatedSecret!,
-  targetDbSecret: targetDbStack.generatedSecret!
 });
 
 new FargateStack(app, 'FargateStack', {
